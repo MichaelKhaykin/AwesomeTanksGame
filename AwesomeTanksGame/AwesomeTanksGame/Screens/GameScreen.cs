fa718bcb3eb57e06bf3f195ead6412fd1e1f827c
MishaTheMiner <mr.khaykin@gmail.com>
2018-08-05 17:04:24 -0700

    Handling movements better, using Dictionary<Key, Action>, also have rotating as keys a and d, the rotation works, and the tank moves in the direction of how it is rotated

--- AwesomeTanksGame/AwesomeTanksGame/Screens/GameScreen.cs
+++ AwesomeTanksGame/AwesomeTanksGame/Screens/GameScreen.cs
@@ -7,20 +7,53 @@ using MichaelLibrary;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace AwesomeTanksGame.Screens
 {
     public class GameScreen : Screen
     {
+        Texture2D tankTexture;
         Tank heroTank;
+        Sprite[,] fog;
 
         public GameScreen(GraphicsDevice graphics, ContentManager content) : base(graphics, content)
         {
+            int screenWidth = graphics.Viewport.Width;
+            int screenHeight = graphics.Viewport.Height;
+
+            tankTexture = Content.Load<Texture2D>("Tanks/BTR_Tower");
+
+            var fogTexture = Content.Load<Texture2D>("Tanks/BTR_Base");
+
+            Texture2D pixel = new Texture2D(graphics, 1, 1);
+            pixel.SetData(new Color[] { Color.White });
+
+            int x = screenWidth % fogTexture.Width;
+            int y = screenHeight % fogTexture.Height;
+
+            fog = new Sprite[x, y];
+            
+            int xAcc = 0;
+            int yAcc = 0;
+            for (int i = 0; i < fog.GetLength(0); i++)
+            {
+                for (int j = 0; j < fog.GetLength(1); j++)
+                {
+                    fog[i, j] = new Sprite(pixel, new Vector2(xAcc, yAcc), Color.Black, new Vector2(fogTexture.Width, fogTexture.Height));
+                    yAcc += fogTexture.Height;
+                }
+                xAcc += fogTexture.Width;
+                yAcc = 0;
+            }
+
             LoadLevel(graphics, content);
         }
 
         public void LoadLevel(GraphicsDevice graphics, ContentManager content)
         {
+            string filename = $"level{Main.LevelSelected}.json";
+
             //9 is amount of levels
             switch (Main.LevelSelected)
             {
@@ -60,9 +93,37 @@ namespace AwesomeTanksGame.Screens
         public void LevelZero(GraphicsDevice graphics, ContentManager content)
         {
             //Level 0 stuff
-            heroTank = new Tank(Content.Load<Texture2D>("Tanks/BTR_Tower"), new Vector2(100, 200), Color.White, 1f.ToVector2(), Content, graphics);
+            heroTank = new Tank(tankTexture, new Vector2(100, 200), Color.White, 1f.ToVector2(), Content, graphics, Keys.W, Keys.S, Keys.D, Keys.A, (tankTexture.Width, 72));
 
             Sprites.Add(heroTank);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            for (int row = 0; row < fog.GetLength(0); row++)
+            {
+                for(int col = 0; col < fog.GetLength(1); col++)
+                {
+                    if (fog[row, col].HitBox.Intersects(heroTank.HitBox))
+                    {
+                        fog[row, col].Enabled = false;
+                        fog[row, col].IsVisible = false;
+                    }
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            for(int i = 0; i < fog.GetLength(0); i++)
+            {
+                for (int j = 0; j < fog.GetLength(1); j++)
+                {
+                    fog[i, j].Draw(spriteBatch);
+                }
+            }
+            base.Draw(spriteBatch);
         }
     }
 }
--- AwesomeTanksGame/AwesomeTanksGame/Tank.cs
+++ AwesomeTanksGame/AwesomeTanksGame/Tank.cs
@@ -28,56 +28,64 @@ namespace AwesomeTanksGame
     public class Tank : Sprite
     {
         private KeyboardState oldKeyboardState;
-        
+
+        public Dictionary<Keys, Action<GameTime>> Movements;
+
         public int Health { get; set; }
         private TimeSpan ElapsedSpecialPowerTimer = TimeSpan.Zero;
         private TimeSpan SpecialPowerTimer = TimeSpan.FromSeconds(10);
 
         private List<Bullet> Bullets;
-        
+
         private Texture2D bulletTexture;
 
+        private Vector2 forwardVector = Vector2.UnitX;
+        private (int moveSpeed, int spinSpeed) speed;
 
-        public Tank(Texture2D texture, Vector2 position, Color color, Vector2 scale, ContentManager Content, GraphicsDevice graphics) 
+        public override float Rotation
+        {
+            get => base.Rotation;
+            set
+            {
+                base.Rotation = value;
+                forwardVector = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
+            }
+        }
+
+        public Tank(Texture2D texture, Vector2 position, Vector2 scale, ContentManager Content, GraphicsDevice graphics, (int moveSpeed, int spinSpeed) speed)
+            : this(texture, position, Color.White, scale, Content, graphics, Keys.W, Keys.S, Keys.D, Keys.A, speed) { }
+
+        public Tank(Texture2D texture, Vector2 position, Color color, Vector2 scale, ContentManager Content, GraphicsDevice graphics, Keys forward, Keys backward, Keys spinLeft, Keys spinRight, (int moveSpeed, int spinSpeed) speed)
             : base(texture, position, color, scale, null)
         {
             Bullets = new List<Bullet>();
             bulletTexture = Content.Load<Texture2D>("Bullet");
+
+            this.speed = speed;
+
+            Movements = new Dictionary<Keys, Action<GameTime>>
+            {
+                [forward] = new Action<GameTime>((gameTime) => Position += forwardVector * speed.moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds),
+                [backward] = new Action<GameTime>((gameTime) => Position -= forwardVector * speed.moveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds),
+                [spinLeft] = new Action<GameTime>((gameTime) => Rotation += MathHelper.ToRadians(speed.spinSpeed) * (float)gameTime.ElapsedGameTime.TotalSeconds),
+                [spinRight] = new Action<GameTime>((gameTime) => Rotation -= MathHelper.ToRadians(speed.spinSpeed) * (float)gameTime.ElapsedGameTime.TotalSeconds)
+            };
         }
 
         public override void Update(GameTime gameTime, GraphicsDevice graphicsDevice = null)
         {
             KeyboardState keyboardState = Keyboard.GetState();
             
-            var mouseVector = new Vector2(Main.MouseState.X, Main.MouseState.Y);
-            var slope = Position - mouseVector;
-
             ElapsedSpecialPowerTimer += gameTime.ElapsedGameTime;
-
-            if (keyboardState.IsKeyDown(Keys.A) && !oldKeyboardState.IsKeyUp(Keys.A))
-            {
-                Position = new Vector2(Position.X - 1, Position.Y);
-            }
-            if (keyboardState.IsKeyDown(Keys.D) && !oldKeyboardState.IsKeyUp(Keys.D))
-            {
-                Position = new Vector2(Position.X + 1, Position.Y);
-            }
-            if (keyboardState.IsKeyDown(Keys.W) && !oldKeyboardState.IsKeyUp(Keys.W))
-            {
-                Position = new Vector2(Position.X, Position.Y - 1);
-            }
-            if (keyboardState.IsKeyDown(Keys.S) && !oldKeyboardState.IsKeyUp(Keys.S))
-            {
-                Position = new Vector2(Position.X, Position.Y + 1);
-            }
+            
+            HandleMovements(keyboardState, gameTime);
 
             if (Main.MouseState.LeftButton == ButtonState.Pressed && Main.oldMouseState.LeftButton == ButtonState.Released)
             {
                 Bullets.Add(new Bullet(bulletTexture, new Vector2(Position.X, Position.Y), Color.White, 0.05f.ToVector2()));
-                slope.Normalize();
-                Bullets[Bullets.Count - 1].Slope = slope * -1;
+                Bullets[Bullets.Count - 1].Slope = forwardVector;
                 Bullets[Bullets.Count - 1].FlyingTime.Start();
-                Bullets[Bullets.Count - 1].Rotation = Rotation + MathHelper.PiOver2;
+                Bullets[Bullets.Count - 1].Rotation = Rotation;
                 //shooting stuffs
             }
 
@@ -87,15 +95,13 @@ namespace AwesomeTanksGame
                 ElapsedSpecialPowerTimer = TimeSpan.Zero;
             }
 
-            Rotation = (float)Math.Atan2(slope.Y, slope.X) + MathHelper.ToRadians(90);//Some rotation value
-
             oldKeyboardState = keyboardState;
-            
+
             //bullet cleanup
             for (int i = 0; i < Bullets.Count; i++)
             {
                 Bullets[i].Position += Bullets[i].Slope * Bullets[i].Speed;
-                
+
                 if (Bullets[i].FlyingTime.ElapsedMilliseconds > 3000)
                 {
                     Bullets.RemoveAt(i);
@@ -104,6 +110,18 @@ namespace AwesomeTanksGame
             }
 
             base.Update(gameTime, graphicsDevice);
+        }
+
+        public void HandleMovements(KeyboardState keyboardState, GameTime gameTime)
+        {
+            foreach (var currentKey in keyboardState.GetPressedKeys())
+            {
+                if (!Movements.ContainsKey(currentKey))
+                {
+                    continue;
+                }
+                Movements[currentKey](gameTime);
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
